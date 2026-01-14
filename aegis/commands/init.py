056b1e47a0a52ed2c51dc9bd20c2c52e58a6680c
Sebastián Quintero <sq07369@gmail.com>
2026-01-14 16:06:44 -0500

    Update init.py

--- aegis/commands/init.py
+++ aegis/commands/init.py
@@ -1,1 +1,17 @@
+import os
+
+BASE_DIRS = [
+    "keys",
+    "logs",
+    "data",
+    "config"
+]
+
+def run(args):
+    print("[*] Inicializando AegisNode...")
+
+    for d in BASE_DIRS:
+        os.makedirs(d, exist_ok=True)
+
+    print("[+] Estructura base creada")
 
